9ed13dc9b7a8ba274831fae090969ada26b52602
edaagunes <edagunes00@hotmail.com>
2024-12-28 23:59:33 +0300

    Added Offer consume in default

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.DiscountDtos;
+using SignalRWebUI.Dtos.SliderDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
 	public class _DefaultOfferComponentPartial:ViewComponent
 	{
-		public IViewComponentResult Invoke()
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _DefaultOfferComponentPartial(IHttpClientFactory httpClientFactory)
 		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7191/api/Discount");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
+				return View(values);
+			}
 			return View();
 		}
 	}
