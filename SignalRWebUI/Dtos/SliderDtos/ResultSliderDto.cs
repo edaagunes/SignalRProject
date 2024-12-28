0407584b81555abcb37bb8d7459f149d212306a9
edaagunes <edagunes00@hotmail.com>
2024-12-28 23:51:39 +0300

    Added Slider consume in default

---
+++ SignalRWebUI/Dtos/SliderDtos/ResultSliderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRWebUI.Dtos.SliderDtos
+{
+	public class ResultSliderDto
+	{
+		public int SliderId { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+
+	}
+}
