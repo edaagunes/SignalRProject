00348265f36078fd871cbab134c27e705c40c8e8
edaagunes <edagunes00@hotmail.com>
2024-12-29 01:12:27 +0300

    Added home page consumes in default

--- SignalR.DtoLayer/ContactDto/UpdateContactDto.cs
+++ SignalR.DtoLayer/ContactDto/UpdateContactDto.cs
@@ -12,6 +12,9 @@ namespace SignalR.DtoLayer.ContactDto
 		public string Location { get; set; }
 		public string Email { get; set; }
 		public string Phone { get; set; }
+		public string FooterTitle { get; set; }
 		public string FooterDescription { get; set; }
+		public string OpenDays { get; set; }
+		public string OpenHours { get; set; }
 	}
 }
