2de1aedf9b280b00addafb7d3df6bb398c4d0b6e
edaagunes <edagunes00@hotmail.com>
2025-01-12 16:34:12 +0300

    Added product and category dynamic in default

---
+++ SignalRWebUI/ViewComponents/MenuComponents/_MenuCategoryFilterComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.CategoryDtos;
+using SignalRWebUI.Dtos.ProductDtos;
+using System.Net.Http;
+
+namespace SignalRWebUI.ViewComponents.MenuComponents
+{
+	public class _MenuCategoryFilterComponentPartial:ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _MenuCategoryFilterComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> Invoke()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7191/api/Categpry");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
+}
